911d9846acbe10fe88fe8466bdb65a9f7492fb56
SeongJin Oh <guk0230@naver.com>
2023-02-28 21:52:46 +0900

    first commit

---
+++ src/main/java/deploy/board/repository/BoardRepository.java
@@ -0,0 +1,8 @@
+package deploy.board.repository;
+
+import deploy.board.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board,Long> {
+
+}
